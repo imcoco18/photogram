6694fad1eb5f01bd5b6801d168cc160fa44d8503
imcoco18 <coco.jasmine18@icloud.com>
2020-04-30 17:39:31 +0900

    [login]製作中

---
+++ config/routes.rb
@@ -0,0 +1,5 @@
+Rails.application.routes.draw do
+  devise_for :users #URLとしてusersを含むことを示す
+  resources :post_images, only: [:new, :create, :index, :show]
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
